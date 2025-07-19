d605369ac32e903c21145f2e797218aab472bb32
azharfarhann <azharfarhan785@gmail.com>
2025-07-19 16:08:30 +0000

    Weather App Completed using tailwind css.

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 5011;
+const PORT = process.env.PORT || 5011;
 
 app.use(express.json());
 
